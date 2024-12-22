1f1b25b15bf528469df6497121f7a9c6f5f681f8
Nuris <230445@astanait.edu.kz>
2024-12-22 21:42:17 +0500

    done the function AddBook

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Book struct {
+	ID         string
+	Title      string
+	Author     string
+	IsBorrowed bool
+}
+
+type Library struct {
+	Books map[string]Book
+}
+
+func (l *Library) AddBook(book Book) {
+	l.Books[book.ID] = book
+	fmt.Println("Book added successfully.")
+}
+
+func main() {
+	library := Library{Books: make(map[string]Book)}
+	var id, title, author string
+	fmt.Print("Enter Book ID: ")
+	fmt.Scan(&id)
+	fmt.Print("Enter Book Title: ")
+	fmt.Scan(&title)
+	fmt.Print("Enter Book Author: ")
+	fmt.Scan(&author)
+	library.AddBook(Book{ID: id, Title: title, Author: author})
+}
