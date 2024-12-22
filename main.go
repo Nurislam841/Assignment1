cc9d47c434c229a53aa9349c93618382bb1887d1
Nuris <230445@astanait.edu.kz>
2024-12-22 21:45:10 +0500

    done the function AddBook

--- main.go
+++ main.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+func main() {
+	library := Library{Books: make(map[string]Book)}
+	var id, title, author string
+	fmt.Print("Enter Book ID: ")
+	fmt.Scan(&id)
+	fmt.Print("Enter Book Title: ")
+	fmt.Scan(&title)
+	fmt.Print("Enter Book Author: ")
+	fmt.Scan(&author)
+	library.AddBook(Book{ID: id, Title: title, Author: author})
+}
+
 type Book struct {
 	ID         string
 	Title      string
@@ -18,16 +30,4 @@ type Library struct {
 func (l *Library) AddBook(book Book) {
 	l.Books[book.ID] = book
 	fmt.Println("Book added successfully.")
-}
-
-func main() {
-	library := Library{Books: make(map[string]Book)}
-	var id, title, author string
-	fmt.Print("Enter Book ID: ")
-	fmt.Scan(&id)
-	fmt.Print("Enter Book Title: ")
-	fmt.Scan(&title)
-	fmt.Print("Enter Book Author: ")
-	fmt.Scan(&author)
-	library.AddBook(Book{ID: id, Title: title, Author: author})
 }
